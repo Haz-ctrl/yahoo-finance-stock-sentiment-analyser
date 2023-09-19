0e663df9182250669837dec7451f8ca85d960a7c
Hashim Iqbal <iqbalhashim332@gmail.com>
2023-09-19 18:36:12 +0100

    Putting news links in array

--- src/main.py
+++ src/main.py
@@ -1,16 +1,28 @@
 import yfinance as yf
 import json
 
-stock = input("Please enter the stock code you wish to download info on:")
-btc = yf.Ticker("TSLA")
+stock = input("Please enter the stock code you wish to download info on: ")
+ticker = yf.Ticker(stock)
 
 # Current price, price at start of day, high, low, mkt cap, p/e ratio
 # Convert the JSON object to a formatted string
-data = btc.info  # For general information about the company
+data = ticker.info  # For general information about the company
+news = ticker.news
 
 # Convert the data to a formatted JSON string
-pretty_data = json.dumps(data, indent=4)
+pretty_data = json.dumps(news, indent=4)
 
 # Print the formatted data
+print(f"Company name: {data['shortName']}")
+print(f"Current trading price: ${data['currentPrice']}")
+print(f"Price at open: ${data['regularMarketOpen']}")
+print(f"Forward P/E ratio: {data['forwardPE']}")
+print(f"Market cap: ${data['marketCap']}")
 print(f"Day high: ${data['dayHigh']}")
 print(f"Day low: ${data['dayLow']}")
+
+print(f"\nDescription: {data['longBusinessSummary']}")
+
+newsLinks = []
+for item in news:
+    newsLinks.append(item['link'])
