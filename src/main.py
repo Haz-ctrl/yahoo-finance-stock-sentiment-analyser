0f14f010a602eed5b7cd1fcdd177be6ac3cb2d92
Hashim Iqbal <iqbalhashim332@gmail.com>
2023-09-21 13:42:45 +0100

    Adding colours, making output look nicer

--- src/main.py
+++ src/main.py
@@ -1,27 +1,27 @@
 import yfinance as yf
-import json
 import sentimentAnalyser as sa
+from colorama import init, Fore, Style
+
+def print_title(title):
+    print(Fore.YELLOW + Style.BRIGHT + f"\n{'='*40}")
+    print(f"{title:^40}")
+    print(f"{'='*40}" + Style.RESET_ALL)
 
 def main():
+    init(autoreset=True)
 
     while True:
-        stock = input("Please enter the stock code you wish to download info on: ").upper()
+        stock = input(Fore.CYAN + "Please enter the stock code you wish to download info on: " + Style.RESET_ALL).upper()
 
         try:
             ticker = yf.Ticker(stock)
-            # Convert the JSON object to a formatted string
-            data = ticker.info  # For general information about the company
+            data = ticker.info
             news = ticker.news
-            # If the Ticker object is successfully created, break out of the loop
             break
         except Exception as err:
-            print("Invalid stock code. Please enter a valid stock code.")
+            print(Fore.RED + "Invalid stock code. Please enter a valid stock code." + Style.RESET_ALL)
 
-
-    # Convert the data to a formatted JSON string
-    pretty_data = json.dumps(news, indent=4)
-
-    # Print the formatted data
+    print_title("Stock Information")
     info_keys = [
         ("shortName", "Company name"),
         ("currentPrice", "Current trading price"),
@@ -36,18 +36,17 @@ def main():
         try:
             value = data[key]
             if isinstance(value, (int, float)):
-                # Format numbers with 2 decimal places and comma for thousands
                 value = f"${value:,.2f}"
-            print(f"{label}: {value}")
+            print(f"{label}: {Fore.GREEN}{value}{Style.RESET_ALL}")
         except KeyError:
-            print(f"{label}: Data not available")
+            print(f"{label}: {Fore.RED}Data not available{Style.RESET_ALL}")
 
     print("\nDescription:")
     try:
         description = data['longBusinessSummary']
         print(description + "\n")
     except KeyError:
-        print("Description not available")
+        print(Fore.RED + "Description not available" + Style.RESET_ALL)
 
     newsLinks = []
     for item in news:
@@ -55,10 +54,11 @@ def main():
 
     overall_sentiment = sa.get_overall_sentiment(newsLinks)
 
+    print_title("Sentiment Analysis")
     if overall_sentiment == 0.0:
-        print('\nNo valid news articles found for sentiment analysis.')
+        print(Fore.YELLOW + "No valid news articles found for sentiment analysis." + Style.RESET_ALL)
     else:
-        print('\nThe overall sentiment portrayed by the news articles is:', format(overall_sentiment, ".2f"))
+        print(Fore.CYAN + f"The overall sentiment portrayed by the news articles is: {format(overall_sentiment, '.2f')}" + Style.RESET_ALL)
         sa.determineSentiment(overall_sentiment)
 
 if __name__ == "__main__":
