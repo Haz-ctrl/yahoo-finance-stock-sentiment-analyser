447d9d6696d6d0831896b2135a24ac66b474bc44
Hashim Iqbal <iqbalhashim332@gmail.com>
2023-10-22 18:55:13 +0100

    Updated sentiment analysis breakdown to just mention number of positives, neutrals and negatives

--- src/main.py
+++ src/main.py
@@ -11,14 +11,16 @@ def main():
     init(autoreset=True)
 
     while True:
-        stock = input(Fore.CYAN + "Please enter the stock code you wish to download info on: " + Style.RESET_ALL).upper()
+        stock = input(Fore.CYAN + "Please enter the stock code you wish to fetch info on: " + Style.RESET_ALL).upper()
         try:
             ticker = yf.Ticker(stock)
             data = ticker.info
             news = ticker.news
+            if not data:
+                raise ValueError("Stock data not available")
             break
-        except Exception as err:
-            print(Fore.RED + "Invalid stock code. Please enter a valid stock code." + Style.RESET_ALL)
+        except Exception as e:
+            print(Fore.RED + f"{e}. Please enter a valid stock code." + Style.RESET_ALL)
 
     print_title("Stock Information")
     info_keys = [
@@ -50,17 +52,22 @@ def main():
     except KeyError:
         print(Fore.RED + "Description not available" + Style.RESET_ALL)
 
+    print(Fore.GREEN + "Collecting and reading news articles to determine stock sentiment...")
+
     newsLinks = []
     for item in news:
         newsLinks.append(item['link'])
 
-    overall_sentiment = sa.get_overall_sentiment(newsLinks)
+    overall_sentiment = sa.getOverallSentiment(newsLinks)
 
-    print_title("Sentiment Analysis")
-    if overall_sentiment == 0.0:
+    print_title("Sentiment Analysis Results")
+
+    if overall_sentiment['Positive'] == 0 and overall_sentiment['Neutral'] == 0 and overall_sentiment['Negative'] == 0:
         print(Fore.YELLOW + "No valid news articles found for sentiment analysis." + Style.RESET_ALL)
     else:
-        print(Fore.CYAN + f"The overall sentiment portrayed by the news articles is: {format(overall_sentiment, '.2f')}" + Style.RESET_ALL)
+        print(Fore.GREEN + f"Positive: {overall_sentiment['Positive']}")
+        print(Fore.YELLOW + f"Neutral: {overall_sentiment['Neutral']}")
+        print(Fore.RED + f"Negative: {overall_sentiment['Negative']}")
         sa.determineSentiment(overall_sentiment)
 
 if __name__ == "__main__":
--- src/sentimentAnalyser.py
+++ src/sentimentAnalyser.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-def get_article_sentiment(url):
+def getArticleSentiment(url):
     # Send an HTTP GET request to the URL
     response = requests.get(url)
 
@@ -19,58 +19,53 @@ def get_article_sentiment(url):
         analyzer = SentimentIntensityAnalyzer()
         sentiment_scores = analyzer.polarity_scores(text_content)
 
-        # Create a dictionary to hold the data including sentiment analysis results
-        data = {
-            'text_content': text_content,
-            'sentiment_scores': sentiment_scores
-        }
+        # Determine the sentiment label based on the compound score
+        compound_sentiment = sentiment_scores['compound']
+        if compound_sentiment >= 0.05:
+            sentiment_label = "Positive"
+        elif compound_sentiment <= -0.05:
+            sentiment_label = "Negative"
+        else:
+            sentiment_label = "Neutral"
 
-        # Print the compound sentiment score from the data dictionary
-        compound_sentiment = data['sentiment_scores']['compound']
-        print(f"Reading article: {url}, Sentiment analysis result: {format(compound_sentiment, '.2f')}")
-        return compound_sentiment
-
+        return sentiment_label
     else:
-        # print(f"Failed to retrieve content from {url}. Status code: {response.status_code}")
+        # Return None for invalid URLs
         return None
 
-def get_overall_sentiment(urls):
-    """Calculates the overall sentiment of an array of news articles."""
-    
-    overall_sentiment = 0.0
+def getOverallSentiment(urls):
+    overall_sentiment = {
+        'Positive': 0,
+        'Neutral': 0,
+        'Negative': 0
+    }
     valid_urls_count = 0  # To keep track of the number of valid URLs
-    
+
     for url in urls:
-        article_sentiment = get_article_sentiment(url)
-        if article_sentiment is not None:
-            overall_sentiment += article_sentiment
+        sentiment_label = getArticleSentiment(url)
+        if sentiment_label is not None:
+            overall_sentiment[sentiment_label] += 1
             valid_urls_count += 1
 
     if valid_urls_count > 0:
-        return round((overall_sentiment / valid_urls_count), 2)
+        return overall_sentiment
     else:
-        return 0.0  # Return 0.0 if no valid URLs were processed
+        return {'Positive': 0, 'Neutral': 0, 'Negative': 0}
     
 def determineSentiment(overall_sentiment):
-    if overall_sentiment < 0.5:
-        sentiment_label = "Negative"
-    elif overall_sentiment < 0.7:
-        sentiment_label = "Balanced"
-    elif overall_sentiment < 1:
-        sentiment_label = "Positive"
+    total_sentiments = overall_sentiment['Positive'] + overall_sentiment['Neutral'] + overall_sentiment['Negative']
+    if total_sentiments > 0:
+        positive_ratio = overall_sentiment['Positive'] / total_sentiments
+        negative_ratio = overall_sentiment['Negative'] / total_sentiments
     else:
-        sentiment_label = "Undetermined"
+        positive_ratio = 0
+        negative_ratio = 0
+
+    if positive_ratio > 0.5:
+        sentiment_label = "Positive"
+    elif negative_ratio > 0.5:
+        sentiment_label = "Negative"
+    else:
+        sentiment_label = "Neutral"
 
     print(f"Overall sentiment: {sentiment_label}")
-
-# Example usage:
-
-# urls = [
-#     'https://www.nytimes.com/2023/09/19/business/economy-recession-fears.html',
-#     'https://www.washingtonpost.com/business/2023/09/19/stock-market-today-dow-jones-nasdaq/',
-#     'https://www.wsj.com/articles/fed-raises-interest-rates-again-in-fight-against-inflation-11663572869'
-# ]
-
-# overall_sentiment = get_overall_sentiment(urls)
-
-# print('The overall sentiment of the news articles is:', overall_sentiment)
